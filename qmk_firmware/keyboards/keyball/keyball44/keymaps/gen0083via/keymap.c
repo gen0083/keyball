51fae1f69fccd31ddf0dc1defb7b1dbe43638028
Gen <archiherewego@gmail.com>
2023-05-10 15:14:56 +0900

    layer2からマウス設定を削除

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT(
-    _______  , KC_PSLS   , KC_P7     , KC_P8       , KC_P9     , KC_PAST     ,         _______        , LGUI(KC_BSPC), KC_HOME   , KC_END         , _______      , _______  ,
-    _______  , KC_PCMM   , KC_P4     , KC_P5       , KC_P6     , KC_PMNS     ,         KC_MS_BTN3     , KC_MS_BTN1   , KC_MS_BTN2, SCRL_MO        , KC_QUOT      , S(KC_2)  ,
-    _______  , KC_BSPC   , KC_P1     , KC_P2       , KC_P3     , KC_PPLS     ,         _______        , KC_MS_BTN4   , KC_MS_BTN5, _______        , _______      , _______  ,
+    _______  , KC_PSLS   , KC_P7     , KC_P8       , KC_P9     , KC_PAST     ,         _______        , _______      , _______   , _______        , _______      , _______  ,
+    _______  , KC_PCMM   , KC_P4     , KC_P5       , KC_P6     , KC_PMNS     ,         _______        , _______      , _______   , _______        , _______      , _______  ,
+    _______  , KC_BSPC   , KC_P1     , KC_P2       , KC_P3     , KC_PPLS     ,         _______        , _______      , _______   , _______        , _______      , _______  ,
                KC_P0     , KC_PDOT   , KC_BSPC     , KC_PEQL   , _______     ,         _______        , _______                                   , _______
   ),
 
