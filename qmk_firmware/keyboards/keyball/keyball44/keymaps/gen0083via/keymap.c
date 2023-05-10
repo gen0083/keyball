caae52173f0821305d9ce674ba3c6efb00535e77
Gen <archiherewego@gmail.com>
2023-05-10 15:31:12 +0900

    Keymapを修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , KC_1      , KC_2      , KC_3        , KC_4      , KC_5        ,         KC_6           , KC_7         , KC_8      , KC_9           , KC_0         , KC_DEL   ,
     KC_CAPS  , S(KC_1)   , S(KC_2)   , S(KC_3)     , S(KC_4)   , S(KC_5)     ,         KC_LEFT        , KC_DOWN      , KC_UP     , KC_RIGHT       , S(KC_SCLN)   , KC_SCLN  ,
     _______  , S(KC_6)   , S(KC_7)   , S(KC_8)     , S(KC_9)   , S(KC_0)     ,         KC_EQL         , S(KC_EQL)    , KC_BSLS   , KC_QUOT        , S(KC_QUOT)   , KC_INT3  ,
-               _______   , _______   , _______     , _______   , _______     ,         SFT_T(KC_LNG1) , GUI_T(KC_LNG2)                            , KC_GRV
+               _______   , _______   , _______     , _______   , _______     ,         SFT_T(KC_LNG1) , GUI_T(KC_LNG2)                            , S(KC_BSLS)
   ),
 
   [2] = LAYOUT(
