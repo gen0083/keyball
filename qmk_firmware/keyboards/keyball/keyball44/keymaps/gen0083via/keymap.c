6132cd018999d396d4958f7edabd22ab8338c8f1
Gen <archiherewego@gmail.com>
2023-05-31 14:36:02 +0900

    キーマップを修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/gen0083via/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT(
     KC_ESC    , KC_Q     , KC_W      , KC_E        , KC_R      , KC_T        ,         KC_Y           , KC_U         , KC_I      , KC_O           , KC_P         , KC_BSPC  ,
-    KC_TAB    , KC_A     , KC_S      , KC_D        , LT(2,KC_F),KC_G         ,         KC_H           , KC_J         , KC_K      , KC_L           , KC_LBRC      , KC_RBRC  ,
+    KC_TAB    , KC_A     , KC_S      , KC_D        , LT(2,KC_F),KC_G         ,         KC_H           , KC_J         , KC_K      , LT(5,KC_L)     , KC_LBRC      , KC_RBRC  ,
     KC_LCTL   , KC_Z     , KC_X      , KC_C        , KC_V      , KC_B        ,         KC_N           , KC_M         , KC_COMM   , KC_DOT         , KC_SLSH      , S(KC_MINS)  ,
                 KC_LALT  , KC_LGUI   , KC_LSFT     , MO(1)     , LT(3,KC_MINS),        SFT_T(KC_ENT)  , KC_SPC                                , LT(2, KC_GRV)
   ),
