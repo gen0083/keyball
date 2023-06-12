6be8709779c97b35547914358ab98010d9d07176
Gen <archiherewego@gmail.com>
2023-06-12 22:30:08 +0900

    タッピングタームを調整

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
@@ -1,11 +1,15 @@
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        // レイヤー切り替えとの併用のためタップの判定値を短めにする
         case LT(1,KC_BSPC):
-            // レイヤー切り替えとの併用のためタップの判定値を短めにす
             return 120;
+        case LT(2,KC_MINS):
+            return 120;
+
+        // liとタイピングするときにずらすように同時押しした場合に反応しないためその対策
         case LT(5,KC_L):
-            // liとタイピングするときにずらすように同時押しした場合に反応しないためその対策
             return TAPPING_TERM + 200;
+
         default:
             return TAPPING_TERM;
     }
