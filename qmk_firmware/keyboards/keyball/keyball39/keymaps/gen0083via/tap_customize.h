fd95e9f77510ffa034a50d2c060fa7d55e528981
Gen <archiherewego@gmail.com>
2023-06-12 18:41:05 +0900

    liと流れるように入力した場合に反応しない問題に対処

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
@@ -3,6 +3,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case LT(1,KC_BSPC):
             // 100だとかなり短いので、このあたりが妥当？
             return 120;
+        case LT(5,KC_L):
+            // liとタイピングするときにずらすように同時押しした場合に反応しないためその対策
+            return TAPPING_TERM + 200;
         default:
             return TAPPING_TERM;
     }
