1d89327bd88194198033d3e8c1c91a58c50a6d87
Gen <archiherewego@gmail.com>
2023-06-12 22:01:28 +0900

    Fix comment

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/tap_customize.h
@@ -1,7 +1,7 @@
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(1,KC_BSPC):
-            // 100だとかなり短いので、このあたりが妥当？
+            // レイヤー切り替えとの併用のためタップの判定値を短めにす
             return 120;
         case LT(5,KC_L):
             // liとタイピングするときにずらすように同時押しした場合に反応しないためその対策
