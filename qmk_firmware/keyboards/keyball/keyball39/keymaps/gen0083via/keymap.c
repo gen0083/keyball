4370315ebbddc905d10c0cea7f95295db64d7f62
Gen <archiherewego@gmail.com>
2023-06-14 13:56:52 +0900

    タップダンスを導入

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/gen0083via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD(TD_Q_ESC), KC_W  , KC_E     , KC_R     , LT(6,KC_T) ,                                 KC_Y     , KC_U     , KC_I     , KC_O           , KC_P     ,
     KC_A     , KC_S     , KC_D     , LT(3,KC_F) , KC_G     ,                                 KC_H     , LT(4,KC_J) , KC_K    , LT(5,KC_L)    , KC_DQUO  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                   KC_N     , KC_M     , KC_COMM  , LT(5,KC_DOT)   , KC_SLSH  ,
-    KC_LCTL  , KC_LALT , KC_LGUI  , KC_LSFT  , LT(1,KC_BSPC) , LT(2,KC_MINS),      KC_ENT , KC_SPACE  ,                                        KC_UNDS
+    KC_LCTL  , KC_LALT , KC_LGUI  , KC_LSFT  , LT(1,KC_BSPC) , LT(2,KC_MINS),       KC_ENT , LT(7,KC_SPACE) ,                                  KC_UNDS
   ),
 
   // 記号・シフトで入力される記号
